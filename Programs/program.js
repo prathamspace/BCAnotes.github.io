cd29e5a5d6fb55d86c2d1053b48abb279c0fdfa1
[hashninja69] <bawaonwork@gmail.com>
2022-04-12 13:32:16 +0530

    practical file of c is added

---
+++ Programs/program.js
@@ -0,0 +1,602 @@
+function copyProgram1(ev) {
+    let msg = document.getElementById("btn1");
+
+    console.log("copied");
+    let div = document.getElementById("code1");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram2(ev) {
+    let msg = document.getElementById("btn2");
+
+    console.log("copied");
+    let div = document.getElementById("code2");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram3(ev) {
+    let msg = document.getElementById("btn3");
+
+    console.log("copied");
+    let div = document.getElementById("code3");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram4(ev) {
+    let msg = document.getElementById("btn4");
+
+    console.log("copied");
+    let div = document.getElementById("code4");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram5(ev) {
+    let msg = document.getElementById("btn5");
+
+    console.log("copied");
+    let div = document.getElementById("code5");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram5(ev) {
+    let msg = document.getElementById("btn6");
+
+    console.log("copied");
+    let div = document.getElementById("code6");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram6(ev) {
+    let msg = document.getElementById("btn6");
+
+    console.log("copied");
+    let div = document.getElementById("code6");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram7(ev) {
+    let msg = document.getElementById("btn7");
+
+    console.log("copied");
+    let div = document.getElementById("code7");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram8(ev) {
+    let msg = document.getElementById("btn8");
+
+    console.log("copied");
+    let div = document.getElementById("code8");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+
+  function copyProgram9(ev) {
+    let msg = document.getElementById("btn9");
+
+    console.log("copied");
+    let div = document.getElementById("code9");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram10(ev) {
+    let msg = document.getElementById("btn10");
+
+    console.log("copied");
+    let div = document.getElementById("code10");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram11(ev) {
+    let msg = document.getElementById("btn11");
+
+    console.log("copied");
+    let div = document.getElementById("code11");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram12(ev) {
+    let msg = document.getElementById("btn12");
+
+    console.log("copied");
+    let div = document.getElementById("code12");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram13(ev) {
+    let msg = document.getElementById("btn13");
+
+    console.log("copied");
+    let div = document.getElementById("code13");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram14(ev) {
+    let msg = document.getElementById("btn14");
+
+    console.log("copied");
+    let div = document.getElementById("code14");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram15(ev) {
+    let msg = document.getElementById("btn15");
+
+    console.log("copied");
+    let div = document.getElementById("code15");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram16(ev) {
+    let msg = document.getElementById("btn16");
+
+    console.log("copied");
+    let div = document.getElementById("code16");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram17(ev) {
+    let msg = document.getElementById("btn17");
+
+    console.log("copied");
+    let div = document.getElementById("code17");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram18(ev) {
+    let msg = document.getElementById("btn18");
+
+    console.log("copied");
+    let div = document.getElementById("code18");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram19(ev) {
+    let msg = document.getElementById("btn19");
+
+    console.log("copied");
+    let div = document.getElementById("code19");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram20(ev) {
+    let msg = document.getElementById("btn20");
+
+    console.log("copied");
+    let div = document.getElementById("code20");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram21(ev) {
+    let msg = document.getElementById("btn21");
+
+    console.log("copied");
+    let div = document.getElementById("code21");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram22(ev) {
+    let msg = document.getElementById("btn22");
+
+    console.log("copied");
+    let div = document.getElementById("code22");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram24(ev) {
+    let msg = document.getElementById("btn24");
+
+    console.log("copied");
+    let div = document.getElementById("code24");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram25(ev) {
+    let msg = document.getElementById("btn25");
+
+    console.log("copied");
+    let div = document.getElementById("code25");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram26(ev) {
+    let msg = document.getElementById("btn26");
+
+    console.log("copied");
+    let div = document.getElementById("code26");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram27(ev) {
+    let msg = document.getElementById("btn27");
+
+    console.log("copied");
+    let div = document.getElementById("code27");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram28(ev) {
+    let msg = document.getElementById("btn28");
+
+    console.log("copied");
+    let div = document.getElementById("code28");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram29(ev) {
+    let msg = document.getElementById("btn29");
+
+    console.log("copied");
+    let div = document.getElementById("code29");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+  function copyProgram30(ev) {
+    let msg = document.getElementById("btn30");
+
+    console.log("copied");
+    let div = document.getElementById("code30");
+    let text = div.innerText;
+    let textArea = document.createElement("textarea");
+    textArea.width = "1px";
+    textArea.height = "1px";
+    textArea.background = "transparents";
+    textArea.value = text;
+    document.body.append(textArea);
+    textArea.select();
+    document.execCommand("copy"); //No i18n
+    document.body.removeChild(textArea);
+    msg.innerText = "copied";
+    setTimeout(() => {
+      msg.innerText = "Click to copy";
+    }, 1000);
+  }
+ 
